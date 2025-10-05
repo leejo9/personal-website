c112deafbd8a668d4685f852d9bb7fcdf1494b39
leejo9 <minescoop04@gmail.com>
2025-10-04 20:05:36 -0400

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import './index.css'
 import App from './App.tsx'
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import StartTop from './components/pagetopfix';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter >
       <StartTop />
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </React.StrictMode>
 );
